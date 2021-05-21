84301792614a2a0b395569e921a232b6e013fd86
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-05-21 14:48:00 +0900

    [add]JS-2

--- JS-1/app.js
+++ JS-1/app.js
@@ -1,1 +1,12 @@
-alert('Hello World');
+var hello = 'Hello World';
+alert(hello);
+
+alert('Hello'+'World'+'（２回目）')
+
+//２つ並べるとアラートが２回でた
+
+var str1 = 'Hello';
+var str2 = 'World!!';
+alert(str1 + str2);
+
+// 変数を入れて加法
